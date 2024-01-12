07df136bbcb2f61a712a8f64d615214350edb194
shyynux <shyynux@gmail.com>
2024-01-12 22:28:46 +0530

    fixed api calls

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import dotenv from "dotenv";
+import { Podcast } from "./podcast";
 
 dotenv.config();
+
+export let data: Podcast[] = [];
 
 const options = {
   method: 'GET',
@@ -32,4 +35,44 @@ export async function callSpotifyPodcastAPI(){
   }
 }
 
-callSpotifyPodcastAPI();
+export async function callAPIbyGenre() {
+const options = {
+  method: 'GET',
+  url: 'https://spotify23.p.rapidapi.com/genre_view/',
+  params: {
+    id: '0JQ5DAqbMKFQRiNGmKYj3B',
+    content_limit: '25',
+    limit: '25'
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.KEY,
+    'X-RapidAPI-Host': process.env.HOST
+  }
+};
+
+try {
+	const response = await axios.request(options);
+  const podcastsResponse = response.data.content.items[1].content;
+  for(let index = 0; index < 25; index++){
+   const id = podcastsResponse.items[index].id;
+   const name = podcastsResponse.items[index].name;
+   const artist = podcastsResponse.items[index].publisher;
+   const image = podcastsResponse.items[index].images[1].url;
+   const uri = podcastsResponse.items[index].uri;
+   const newPodcast: Podcast = {
+    id,
+    name,
+    artist,
+    uri,
+    image
+   };
+   data.push(newPodcast);
+  }
+  console.log(data);
+} catch (error) {
+	console.error(error);
+}
+}
+
+// callSpotifyPodcastAPI();
+callAPIbyGenre();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import express from 'express';
 import dotenv from "dotenv";
-
-import axios from 'axios';
-dotenv.config();
+import path from "path";
 
 console.log(" hello podcast listeners!!! ");
+
+const app = express();
+const port = 3000; 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (request: any, response: any) => {
+    response.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+/* cache search for 1 hour = 3600s */
+
+console.log("test");
